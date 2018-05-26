7b6ad0f06fa757f1ff22695cf77cf26e936f4251
Ioshiro <fallenioshiro@gmail.com>
2018-05-26 17:52:46 +0200

    Add files via upload

--- log/log.c
+++ log/log.c
@@ -29,6 +29,8 @@ void end_time () {
   char* end_str = "End time = ";
   char* time_spent_str = "Time spent processing = ";
   char* value_str = "Value = ";
+  int lung = strlen(c_path);
+  char format[4] = {c_path[lung-3], c_path[lung-2], c_path[lung-1], 0};
   tend = time(NULL);
   double ttot = difftime(tend,tstart);
   strftime(buf,20,"%Y-%m-%d %H:%M:%S",localtime(&tstart));
@@ -46,6 +48,7 @@ void end_time () {
   path = open("/tmp/path.tmp", O_RDONLY);
   char c_path [i];
   read(path,c_path,i);
+  c_path[i]= 00;
   printf("Ho letto cosi': %s \n",c_path);
   printf("Secondo i = %i \n", i);
   close(path);
@@ -54,18 +57,54 @@ void end_time () {
   close(value);
   //stampo i dati nel file di output
   fd[0] = open(c_path , O_WRONLY | O_APPEND | O_CREAT, 0777);
-  write(fd[0], start_str, strlen(start_str));
-  write(fd[0], buf, strlen(buf));
-  write(fd[0], "\n", 1);
-  write(fd[0], end_str, strlen(end_str));
-  write(fd[0], buf2, strlen(buf2));
-  write(fd[0], "\n", 1);
-  write(fd[0], time_spent_str, strlen(time_spent_str));
-  write(fd[0], buftot, strlen(buftot));
-  write(fd[0], "\n", 1);
-  write(fd[0], value_str, strlen(value_str));
-  write(fd[0], c_value, strlen(c_value));
-  close(fd[0]);
+  if(strcmp(format,"txt")) {
+    write(fd[0], start_str, strlen(start_str));
+    write(fd[0], buf, strlen(buf));
+    write(fd[0], "\n", 1);
+    write(fd[0], end_str, strlen(end_str));
+    write(fd[0], buf2, strlen(buf2));
+    write(fd[0], "\n", 1);
+    write(fd[0], time_spent_str, strlen(time_spent_str));
+    write(fd[0], buftot, strlen(buftot));
+    write(fd[0], "\n", 1);
+    write(fd[0], value_str, strlen(value_str));
+    write(fd[0], c_value, strlen(c_value));
+    close(fd[0]);
+    exit(0);
+  }else if(strcmp(format,"csv")){
+    write(fd[0], start_str, strlen(start_str));
+    write(fd[0], buf, strlen(buf));
+    write(fd[0], ";\\n\n", 5);
+    write(fd[0], end_str, strlen(end_str));
+    write(fd[0], buf2, strlen(buf2));
+    write(fd[0], ";\\n\n", 5);
+    write(fd[0], time_spent_str, strlen(time_spent_str));
+    write(fd[0], buftot, strlen(buftot));
+    write(fd[0], ";\\n\n", 5);
+    write(fd[0], value_str, strlen(value_str));
+    write(fd[0], c_value, strlen(c_value));
+    write(fd[0], "\\n", 3);
+    close(fd[0]);
+    exit(0);
+  }else if(strcmp(format,"xml")){
+    write(fd[0], "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n", MAX);
+    write(fd[0], "<Start time>", 20);
+    write(fd[0], buf, strlen(buf));
+    write(fd[0], "</Start time>\n", 20);
+    write(fd[0], "<End time>", 20);
+    write(fd[0], buf2, strlen(buf2));
+    write(fd[0], "</End time>\n", 20);
+    write(fd[0], "<Time spent processing>", 30);
+    write(fd[0], buftot, strlen(buftot));
+    write(fd[0], "</Time spent processinge>\n", 30);
+    write(fd[0], "<Value>", 20);
+    write(fd[0], c_value, strlen(c_value));
+    write(fd[0], "</Value>", 20);
+    close(fd[0]);
+    exit(0);
+  }else {
+    printf("Formato non supportato\n");
+  }
   printf("\n **SIGUSR2 called and done** \n");
   return;
 }
@@ -74,11 +113,13 @@ int main () {
         //invio il pid al main e aspetto che mi risponda
 	pid_t pid;
 	pid = getpid();
-	int pid_log = open("/tmp/log.tmp", O_WRONLY | O_CREAT, 0777);
 	char c_pid [MAX];
 	sprintf(c_pid, "%i", pid);
+
+	int pid_log = open("/tmp/log.tmp", O_WRONLY | O_CREAT, 0777);
 	write(pid_log, c_pid, strlen(c_pid));
 	close(pid_log);
+  
 	//calcolo il tempo iniziale quando il main mi dice di iniziare
 	signal(SIGUSR1, start_time);
 	//calcolo il tempo finale quando il main mi "sveglia"
