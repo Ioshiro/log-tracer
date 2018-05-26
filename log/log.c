c78d57b4e3989f5aefab8fc8f4e9319d67447144
Ioshiro <fallenioshiro@gmail.com>
2018-05-26 17:37:53 +0200

    Add files via upload

--- log/log.c
+++ log/log.c
@@ -1,4 +1,4 @@
-#include<signal.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -9,62 +9,82 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #define MAX 100
+#define MAX_TIME 20
 
 
 time_t tstart,tend;
 
-void StartTime (int sig) {
-//inizializzo il tempo iniziale
-		tstart = time(NULL);
-
+void start_time (int sig) {
+  //inizializzo il tempo iniziale
+  tstart = time(NULL);
+  printf("\n **SIGUSR1 called and done** \n");
+  return;
 }
 
-void EndTime () {
-//inizializzo il tempo di fine e stampo nel file le cose
-	tend = time(NULL);
-	double ttot = difftime(tend,tstart);
-	char cttot[MAX];
-	snprintf(cttot,20, "%f", ttot);
-	char buf[20], buf2[20], buftot[20];
-	strftime(buf,20,"%Y-%m-%d %H:%M:%S",localtime(&tstart));
-	strftime(buf2,20,"%Y-%m-%d %H:%M:%S",localtime(&tend));
-	printf("buff: %s \n",buf);
-	//sprintf(buftot,"%f", ttot);
-	int fd[2];
-	char* start_str = "Start time = ";
-	char* end_str = "End time = ";
-	char* time_spent_str = "Time spent processing: ";
-	fd[0] = open("/home/ale/Documenti/logger/log-tracer", O_WRONLY | O_CREAT, 0777);
-	write(fd[0], start_str, strlen(start_str));
-	write(fd[0], buf, strlen(buf));
-	write(fd[0], "\n", 1);
-	write(fd[0], end_str, strlen(end_str));
-	write(fd[0], buf2, strlen(buf2));
-	write(fd[0], "\n", 1);
-	write(fd[0], time_spent_str, strlen(time_spent_str));
-	write(fd[0], buftot, strlen(buftot)); // **QUESTO NON VA**
+void end_time () {
+  //inizializzo il tempo finale e calcolo il tempo di esecuzione
+  int fd[2];
+  char buf[MAX_TIME], buf2[MAX_TIME], buftot[MAX_TIME], c_value[2];
+  char* start_str = "Start time = ";
+  char* end_str = "End time = ";
+  char* time_spent_str = "Time spent processing = ";
+  char* value_str = "Value = ";
+  tend = time(NULL);
+  double ttot = difftime(tend,tstart);
+  strftime(buf,20,"%Y-%m-%d %H:%M:%S",localtime(&tstart));
+  strftime(buf2,20,"%Y-%m-%d %H:%M:%S",localtime(&tend));
+  snprintf(buftot,20, "%f", ttot);
+  //mi viene passato il path del file di output
+  int path = open("/tmp/path.tmp", O_RDONLY);
+  char c_pathtmp[MAX];
+  //char c_empty[MAX]="";
+  int i = read(path,c_pathtmp,MAX);
+  printf("Ho letto cosi': %s \n",c_pathtmp);
+  //strcpy(c_path,c_empty);
+  close(path);
+  printf("Primo i = %i \n", i);
+  path = open("/tmp/path.tmp", O_RDONLY);
+  char c_path [i];
+  read(path,c_path,i);
+  printf("Ho letto cosi': %s \n",c_path);
+  printf("Secondo i = %i \n", i);
+  close(path);
+  int value = open("/tmp/rvalue.tmp", O_RDONLY);
+  read(value, c_value, 2);
+  close(value);
+  //stampo i dati nel file di output
+  fd[0] = open(c_path , O_WRONLY | O_APPEND | O_CREAT, 0777);
+  write(fd[0], start_str, strlen(start_str));
+  write(fd[0], buf, strlen(buf));
+  write(fd[0], "\n", 1);
+  write(fd[0], end_str, strlen(end_str));
+  write(fd[0], buf2, strlen(buf2));
+  write(fd[0], "\n", 1);
+  write(fd[0], time_spent_str, strlen(time_spent_str));
+  write(fd[0], buftot, strlen(buftot));
+  write(fd[0], "\n", 1);
+  write(fd[0], value_str, strlen(value_str));
+  write(fd[0], c_value, strlen(c_value));
   close(fd[0]);
-	exit(0);
+  printf("\n **SIGUSR2 called and done** \n");
+  return;
 }
 
 int main () {
+        //invio il pid al main e aspetto che mi risponda
 	pid_t pid;
 	pid = getpid();
-	int pid_log = open("/tmp/log", O_WRONLY | O_CREAT, 0777);
+	int pid_log = open("/tmp/log.tmp", O_WRONLY | O_CREAT, 0777);
 	char c_pid [MAX];
 	sprintf(c_pid, "%i", pid);
 	write(pid_log, c_pid, strlen(c_pid));
 	close(pid_log);
-	signal(SIGUSR1, StartTime);
-	//kill(pid,SIGUSR1);
-	signal(SIGUSR2, EndTime);
-	//do {
-		//sleep(3);
-	//} while(0);
-	//kill(pid,SIGUSR2);
-
-	//exit(EXIT_SUCCESS);
+	//calcolo il tempo iniziale quando il main mi dice di iniziare
+	signal(SIGUSR1, start_time);
+	//calcolo il tempo finale quando il main mi "sveglia"
+	signal(SIGUSR2, end_time);
+	//dormo aspettando il main
 	while (1)
 		sleep(1);
-	return 0;
+	exit(EXIT_SUCCESS);
 }
--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #define MAX 5
 
+char* pipe_toks[3];
 
 char* path_tokenize(char* old_path){
 	char* new_path;
@@ -17,7 +18,7 @@ char* path_tokenize(char* old_path){
 		return new_path;
 	}
 	else{
-		perror("Sintassi di path errata. Usare --logfile=path \n");
+		perror("Sintassi di percorso errata. Usare --logfile=path \n");
 		exit(EXIT_FAILURE);
 	}
 }
@@ -32,12 +33,12 @@ char* format_tokenize(char* old_format){
 		return new_format;
 	}
 	else{
-		perror("Sintassi di format errata. Usare --format=txt/csv \n");
+		perror("Sintassi di formato errata. Usare --format=txt/csv \n");
 		exit(EXIT_FAILURE);
 	}
 }
 
-void path_save(char* path){
+void path_write(char* path){
 	int fd[2];
 	fd[0] = open("/tmp/path.tmp", O_WRONLY | O_CREAT, 0777);
 	write(fd[0], path, strlen(path));
@@ -45,7 +46,7 @@ void path_save(char* path){
 	return;
 }
 
-void format_save(char* format){
+void format_write(char* format){
 	int fd[2];
 	fd[0] = open("/tmp/format.tmp", O_WRONLY | O_CREAT, 0777);
 	write(fd[0], format, strlen(format));
@@ -53,26 +54,81 @@ void format_save(char* format){
 	return;
 }
 
+void r_value_write(char* argv){
+	int r = WEXITSTATUS(system(argv));
+	int fd[2];
+	fd[0]= open("/tmp/rvalue.tmp", O_WRONLY | O_CREAT, 0777);
+	char r_value[MAX];
+	sprintf(r_value, "%i \n", r);
+	write(fd[0], r_value, strlen(r_value));
+	close(fd[0]);
+	return;
+}
+
+void command_write(char* command){
+	int fd[2];
+	fd[0] = open("/tmp/command.tmp", O_WRONLY | O_CREAT, 0777);
+	write(fd[0], command, strlen(command));
+	close(fd[0]);
+	return;
+}
+
+int pipe_check (char* command){
+	char c = '|';
+	for(int i=0; i < strlen(command); i++){
+		if(command[i] == c){
+			return 1; // pipe-run
+		}
+	}
+	return 0; // solo-run
+}
+
+void pipe_tokenize(char* pipeline){
+	char c[2] = "|";
+	pipe_toks[0] = strtok(pipeline,c);
+	pipe_toks[1] = strtok(NULL,c);
+	return;
+}
+
+
 int main (int argc, char** argv) {
-	if (argc!=3){
+	if (argc!=4){
 		perror("Sinstassi errata. La sintassi corretta e': ./run --pathfile=path --format=frmt comandi");
 		exit(EXIT_FAILURE);
 	}
-	int pid_log = open("/tmp/log", O_RDONLY);
+
+	int pid_log = open("/tmp/log.tmp", O_RDONLY);
 	char c_log[MAX];
 	char* path_tok;
 	char* format_tok;
+
 	read(pid_log, c_log, MAX);
-//	printf("%s\n",c_log);
 	close(pid_log);
-//	int pid = atoi(c_log);
-//	kill(pid, SIGUSR1);
-//	kill(pid, SIGUSR2);
+	int pid = atoi(c_log);
+	printf("%i\n",pid);
+
+	kill(pid, SIGUSR1);
+
 	path_tok = path_tokenize(argv[1]);
+	path_write(path_tok);
 	format_tok = format_tokenize(argv[2]);
-	path_save(path_tok);
-	format_save(format_tok);
+	format_write(format_tok);
 	printf("Tok1: %s \n",path_tok);
 	printf("Tok2: %s \n",format_tok);
-	return 0;
+
+	int cpipe = pipe_check(argv[3]);
+		if(cpipe == 0){
+			printf("Solo-run! \n");
+			r_value_write(argv[3]);
+		}
+		else{
+			printf("Wombo-combo! \n");
+			pipe_tokenize(argv[3]);
+			r_value_write(pipe_toks[0]);
+			r_value_write(pipe_toks[1]);
+		}
+	command_write(argv[3]);
+
+	kill(pid, SIGUSR2);
+	exit(EXIT_SUCCESS);
 }
